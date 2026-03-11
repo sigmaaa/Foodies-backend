6b4d844e9b724f257b501e8bddb3200c2335083e
Artem Hordiyevskyy <hordiyevskyy@gmail.com>
2026-03-11 12:33:17 +0200

    repo init

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import prettier from 'eslint-config-prettier';
+
+export default defineConfig([
+    {
+        files: ['**/*.{js,mjs,cjs}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+        languageOptions: {
+            globals: globals.node,
+            ecmaVersion: 'latest',
+        },
+    },
+    {
+        files: ['**/*.js'],
+        languageOptions: { sourceType: 'module' },
+    },
+    prettier,
+]);
